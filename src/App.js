7d6cfa94b3dd5ae8af7bafd9ebafff9c12f5fa29
ShindanoSingi <shindanosingi@sekur.com>
2022-12-11 23:59:32 -0500

    added grids

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [inputAppear, setInputAppear] = useState(false)
+  const[input, setInput] = useState(0)
+
+  let num = input;
+  let array = []
+
+  function changeInputAppearance(){
+    setInputAppear(true)
+  }
+
+  function onInputChange(event){
+    setInput(event.target.value);
+  }
+
+  function onSubmit(event){
+    event.preventDefault();
+     console.log(input)
+  }
+
+  if (num < 2 || num === null || num === undefined || num > 10) {
+    alert('Please enter a number between 2 and 5')
+  } else {
+    num *= num
+    for (let i = 0; i < num; i++) {
+      array.push(i + 1)
+    }
+    console.log(array)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <header className="header">Image Matching Game</header>
+      <main className="main" style={{display: 'grid', gridTemplateColumns: `repeat(${input}, 5em)` }}>
+        {!inputAppear ? (
+          <label>
+            Enter the number between 2 and 4:
+            <div className="input-and-button">
+              <input type='integer' maxLength={2} onChange={onInputChange} />
+              <button onClick={(event) => {
+              onSubmit(event);
+              changeInputAppearance()}}>OK</button>
+            </div>
+          </label>
+        ) : (
+          array.map((item, key) => (
+            <div className="item" key={key}>
+              {item}
+            </div>
+          ))
+        )}
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
